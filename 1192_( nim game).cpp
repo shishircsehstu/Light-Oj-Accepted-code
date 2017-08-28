dd915e94a7509669bde4ecdd8dd780174f563bb8
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2017-08-28 12:41:50 -0700

    Create 1192_( nim game).cpp

---
+++ 1192_( nim game).cpp
@@ -0,0 +1,103 @@
+/******************************************************************
+ *                   BISMILLAHIR RAHMANIR RAHIM                   *
+ *                     Saddam Hossain shishir                     *
+ *       Hajee Mohammad Danesh Science & Technology University    *
+ *                                                                *
+ ***************************************************************** */
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<map>
+#include<string>
+#include<set>
+#include<cmath>
+#include<cctype>
+#include<stack>
+#include<cstdlib>
+#include<utility>
+#include<vector>
+#include<deque>
+#include<queue>
+#include<algorithm>
+
+#define sc scanf
+#define si(t) scanf("%d",&t)
+#define sl(t) scanf("%I64d",&t)
+#define sii(a,b) scanf("%d%d",&a,&b)
+
+#define P(a) printf("%d\n",a)
+#define PLN(a) printf("%I64d ",a)
+#define pf printf
+
+#define gcd(a,b) __gcd(a,b)
+#define ff first
+#define ss second
+#define pr1(a) cout<<a<<endl
+#define pr2(a,b) cout<<a<<" "<<b<<endl
+#define pb push_back
+#define pii pair<int,int>
+#define mk make_pair
+#define pi acos(-1.0)
+#define PI 3.1415926535897932385
+#define Sin(a) sin((pi*a)/180)
+#define siz 1000001
+#define mem(ar) memset(ar,0,sizeof ar)
+#define one(x) __builtin_popcount(x)
+typedef long long ll;
+using namespace std;
+
+//int dx[]= {-1,-1,0,0,1,1};
+//int dy[]= {-1,0,-1,1,0,1};
+//int dx[]= {0,0,1,-1};/*4 side move*/
+//int dy[]= {-1,1,0,0};/*4 side move*/
+//int dx[]= {1,1,0,-1,-1,-1,0,1};/*8 side move*/
+//int dy[]= {0,1,1,1,0,-1,-1,-1};/*8 side move*/
+//int dx[]={1,1,2,2,-1,-1,-2,-2};/*knight move*/
+//int dy[]={2,-2,1,-1,2,-2,1,-1};/*knight move*/
+
+//'A'=65,'Z'=90 'a'=97 'z'=122 '0'=48
+
+
+map<ll,bool>visi;
+int Set(int N,int pos)
+{
+    return N=N | (1<<pos);
+}
+int reset(int N,int pos)
+{
+    return N= N & ~(1<<pos);
+}
+bool check(int N,int pos)
+{
+    return (bool)(N & (1<<pos));
+}
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    ll  i,j,m,a,c,n,b,d,ck,t,r,x,ans,rem,y,cnt,lo,hi,cs=1;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        scanf("%lld",&n);
+        r=0;
+        ans=0;
+        for(i=0; i<n; i++)
+        {
+            scanf("%lld%lld",&x,&y);
+            ans=ans^(y-x-1);
+        }
+        if(ans)
+            printf("Case %lld: Alice\n",cs++);
+        else
+            printf("Case %lld: Bob\n",cs++);
+
+    }
+}
+/*
+6 0
+-1 2 -3 4 -5 6
+5 0
+-7 -3 -2 5 8
+
+*/
