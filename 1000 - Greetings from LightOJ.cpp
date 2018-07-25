7941e47bea1dd5970143f9501c62ef5b97af6481
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2018-07-25 23:54:15 +0600

    Create 1000 - Greetings from LightOJ.cpp

---
+++ 1000 - Greetings from LightOJ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cin >> n;
+int i = 0;
+while(n--){
+        i++;
+    int a,b;
+    cin >> a >>b;
+    cout << "Case "<<i<<": "<<a + b;
+    cout << endl;
+}
+}
